747d723ed00e40d28cd1dd8a665adccbc21b4f5b
yuchi <yuchiehtn@gmail.com>
2021-11-17 22:29:16 +0800

    do RegisterActivity.kt & MainActivity.kt

--- app/src/main/java/com/wuhome/firebaseauthdemo/SecondFragment.kt
+++ app/src/main/java/com/wuhome/firebaseauthdemo/SecondFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_first.*
 
 
-class SecondFragment : Fragment(R.layout.fragment_first) {
+class SecondFragment : Fragment(R.layout.fragment_second) {
 
 
 }
