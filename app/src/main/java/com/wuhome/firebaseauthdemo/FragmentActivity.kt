f90b63a8cddd53db9c49e12476ea956beaad2cab
yuchi <yuchiehtn@gmail.com>
2021-11-18 22:22:04 +0800

    finish fragment changing

--- app/src/main/java/com/wuhome/firebaseauthdemo/FragmentActivity.kt
+++ app/src/main/java/com/wuhome/firebaseauthdemo/FragmentActivity.kt
@@ -2,10 +2,37 @@ package com.wuhome.firebaseauthdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        //fragment transaction
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)//指定framelayout要放哪個fragment
+            commit()
+        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                //使用replace來切換fragment時，fragment是沒有stack的(不是on top of)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
